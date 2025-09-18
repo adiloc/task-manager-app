38a5031fcd650ba05a8a389b441c846f0933b1a4
adiloc <adrian.mihai.dumitrascu@gmail.com>
2025-09-19 00:07:59 +0300

    feat: Set up basic routing in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+
+import Register from './components/Register';
+import Login from './components/Login';
+import TaskList from './components/TaskList';
+
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Navigate to="/register" />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/tasks" element={<ProtectedRoute />}>
+            <Route path="/tasks" element={<TaskList />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
