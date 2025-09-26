431b0b04be6a6edbb54fe1b2d7c2164f169249a1
adiloc <adrian.mihai.dumitrascu@gmail.com>
2025-09-26 14:54:58 +0300

    refactor: Replace Register and Login routes with unified AuthPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
 
-import Register from './components/Register';
-import Login from './components/Login';
-import TaskList from './components/TaskList';
+import AuthPage from "./pages/AuthPage";
+import TaskList from "./components/TaskList";
 
-import ProtectedRoute from './components/ProtectedRoute';
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <div className="container mt-4">
         <Routes>
-          <Route path="/" element={<Navigate to="/register" />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Navigate to="/auth" />} />
+          <Route path="/auth" element={<AuthPage />} />
           <Route path="/tasks" element={<ProtectedRoute />}>
             <Route path="/tasks" element={<TaskList />} />
           </Route>
