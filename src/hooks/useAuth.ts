c45d5c5a10b34cd3e2d5e3142708a541deba68ac
adiloc <adrian.mihai.dumitrascu@gmail.com>
2025-09-27 01:09:19 +0300

    chore: Remove unused import of 'api' to fix ESLint error

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { api } from "../api";
 
 export const useAuth = () => {
   const [error, setError] = useState("");
